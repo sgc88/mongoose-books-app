13ca37322f4716ea2a26288f1c8ccfc92889eb01
sgc88 <sehribancheng@yahoo.com>
2017-10-11 15:26:15 -0700

    first commit

---
+++ models/book.js
@@ -0,0 +1,20 @@
+
+// var mongoose = require('mongoose');
+//
+// var Schema = mongoose.Schema;
+// var BookSchema = new Schema({
+//   title: String,
+//   author: String,
+//   image: String,
+//   releaseDate: Number
+// });
+//
+// //create the book model from the schema
+// var Book = mongoose.model('Book', BookSchema);
+//
+//
+//
+//
+//
+// //exposrt Book from module
+// module.export = Book;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,13 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+
+module.exports.Book = require("./book.js");
+
+  
+// the mongoose.connect line above  needs to happen exactly once in your code
+    // move it from book.js to index.js  :)
+
+module.exports.Gargoyle = require("./gargoyle.js");
+module.exports.Goblin = require("./goblin.js");
+module.exports.Gnome = require("./gnome.js");
